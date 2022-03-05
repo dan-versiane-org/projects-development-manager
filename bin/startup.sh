517178c695a77a41de701ae74461004d4401f0a1
Daniel Versiane <daniel.versiane@gmail.com>
2022-03-04 22:09:30 -0300

    feat: updated on how to run a command

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,10 +4,23 @@ source "$PDM_DIR/.env"
 source "$PDM_DIR/configs/path.env"
 
 do_exec_command() {
-  local command="$1.sh"
-  shift
-  $PDM_SCRIPTS_DIR/$command $@
-  exit $?
+  local script="$PDM_SCRIPTS_DIR/$1.sh"
+
+  if [ ! -f $script ];then
+    echo -e " [\e[1;31mError\e[0m] \e[1;35m${1}\e[0m command not be found."
+    exit 1
+  fi
+
+  source $script
+  command="handle_${1}_${2}"
+
+  if ! type $command &> /dev/null;then
+    echo -e " [\e[1;31mError\e[0m] \e[1;35m${1} ${2}\e[0m could not be found."
+    exit 1
+  fi
+
+  shift 2
+  $command $@
 }
 
 do_exec_command $@
