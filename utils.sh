d0fb00039d75bab7b0a55f19b210787c6cc59640
Daniel Versiane <daniel.versiane@gmail.com>
2022-03-28 10:50:31 -0300

    feat: add util colors variables

--- utils.sh
+++ utils.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
 
+## Sets the variables
 PDM_SCRIPTS_DIR="$PDM_DIR/scripts"
 PDM_SPACE="  "
 PDM_ECHO_SPACE=" "
+
+## Sets the colors variables
+PDM_PC="\e[1;35m" # Primary Color
+PDM_EC="\e[1;31m" # Error Color
+PDM_WC="\e[1;33m" # Warning Color
+PDM_IC="\e[1;34m" # Info Color
+PDM_GC="\e[0;32m" # Green Color
+PDM_TC="\e[4;33m" # Title Color
+PDM_RC="\e[0m" # Reset Color
 
 pdm_has_package() {
   type "$1" > /dev/null 2>&1
@@ -17,23 +27,23 @@ pdm_latest_version() {
 }
 
 pdm_success() {
-  echo -e >&2 "${PDM_ECHO_SPACE}[\e[1;35mSuccess\e[0m]: $1"
+  echo -e >&2 "${PDM_ECHO_SPACE}[${PDM_PC}Success${PDM_RC}]: $1"
 }
 
 pdm_error() {
-  echo -e >&2 "${PDM_ECHO_SPACE}[\e[1;31mError\e[0m]: $1"
+  echo -e >&2 "${PDM_ECHO_SPACE}[${PDM_EC}Error${PDM_RC}]: $1"
 }
 
 pdm_warning() {
-  echo -e >&2 "${PDM_ECHO_SPACE}[\e[1;33mWarning\e[0m]: $1"
+  echo -e >&2 "${PDM_ECHO_SPACE}[${PDM_WC}Warning${PDM_RC}]: $1"
 }
 
 pdm_info() {
-  echo -e >&2 "${PDM_ECHO_SPACE}[\e[1;34mInfo\e[0m]: $1"
+  echo -e >&2 "${PDM_ECHO_SPACE}[${PDM_IC}Info${PDM_RC}]: $1"
 }
 
 pdm_show_command() {
-  printf %s "${PDM_SPACE}\e[0;32m${1}|\e[0m${2}"
+  printf %s "${PDM_SPACE}${PDM_GC}${1}|${PDM_RC}${2}"
 }
 
 pdm_has_current_workspace() {
