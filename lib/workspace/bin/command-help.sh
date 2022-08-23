4397d260f88900c759dfaf314ce2d06736cd1c55
Daniel Versiane <daniel.versiane@gmail.com>
2022-08-23 20:54:11 -0300

    style: lint workspace help

--- lib/workspace/bin/command-help.sh
+++ lib/workspace/bin/command-help.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 workspace::commands() {
-  echo "$(pdm::show_command 'create' 'Create a new workspace')"
-  echo "$(pdm::show_command 'current' 'Shows the current workspace')"
-  echo "$(pdm::show_command 'delete' 'Delete a workspace')"
-  echo "$(pdm::show_command 'edit' 'Edit a workspace')"
-  echo "$(pdm::show_command 'help' 'Show this help')"
-  echo "$(pdm::show_command 'list' 'Show all workspaces')"
-  echo "$(pdm::show_command 'set' 'Set a workspace current')"
+  echo "$(pdm::show_command 'create' 'Create a new workspace.')"
+  echo "$(pdm::show_command 'current' 'Shows the current workspace.')"
+  echo "$(pdm::show_command 'delete' 'Delete a workspace.')"
+  echo "$(pdm::show_command 'edit' 'Edit a workspace.')"
+  echo "$(pdm::show_command 'help' 'Show this help.')"
+  echo "$(pdm::show_command 'list' 'Show all workspaces.')"
+  echo "$(pdm::show_command 'set' 'Set a workspace current.')"
 }
 
 workspace::help() {
