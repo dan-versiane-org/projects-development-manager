eec5c257a26516dc703cfe4f4a23913fc0428f25
Daniel Versiane <daniel.versiane@gmail.com>
2022-08-23 20:39:50 -0300

    refactor: change structure of project

---
+++ lib/workspace/bin/command-delete.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+workspace::delete() {
+  # TODO: in development
+  pdm::echo "in development"
+}
+
+workspace::delete ${@}
---
+++ lib/workspace/bin/command-edit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+workspace::edit() {
+  # TODO: in development
+  pdm::echo "in development"
+}
+
+workspace::edit ${@}
